292c43e3c1f05f172d686a019229e44d9412b0da
Kevin Hanna <kevindhanna@live.com>
2021-01-31 18:02:36 +0000

    refactor file read in Model

--- src/models/model.rb
+++ src/models/model.rb
@@ -3,8 +3,7 @@ class Model
   TALENT = 'talent.json'
 
   def self.all
-    s = File.read(TALENT)
-    talent = JSON.parse(s)
+    talent = read_file
     talent.map do |model|
       new(model['name'], model['location'], model['date_of_birth'])
     end
@@ -13,11 +12,15 @@ class Model
   def self.find_by_location(location)
     return all if location.nil? || location.empty?
 
-    s = File.read(TALENT)
-    talent = JSON.parse(s)
+    talent = read_file
     talent.filter_map do |model|
       new(model['name'], model['location'], model['date_of_birth']) if model['location'].downcase == location.downcase
     end
+  end
+
+  def self.read_file
+    s = File.read(TALENT)
+    JSON.parse(s)
   end
 
   def initialize(name, location, date_of_birth)
@@ -25,4 +28,6 @@ class Model
     @location = location
     @date_of_birth = date_of_birth
   end
+
+  private_class_method :read_file
 end
